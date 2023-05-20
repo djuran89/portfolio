80d3b3523aef477b8cfe3d83adbe706992c8f6da
Aleksandar Djuran <djuran89@gmail.com>
2023-05-20 02:34:03 +0200

    add workpreview

--- components/curriculumVitae/workPreview.js
+++ components/curriculumVitae/workPreview.js
@@ -3,10 +3,10 @@ import IPhone from "@/components/iPhone/iPhone";
 function WorkPreview() {
 	return (
 		<section className="flex justify-center items-center min-h-screen lg:flex-row flex-col">
-			<IPhone url="http://18.195.49.234" add={-500} />
-			<IPhone url="http://18.195.49.234" add={-450} />
-			<IPhone url="http://18.195.49.234" add={-400} />
-			<IPhone url="http://18.195.49.234" add={-350} />
+			<IPhone url="https://korisnaknjiga.com" add={-500} />
+			<IPhone url="http://3.75.204.76/?t=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJSZXN0YXVyYW50IjoiNjJiMzY0Y2FhZTdkNjIzYjRjMjdiYWIxIiwiVGFibGUiOiI2MmIzNjU1ZDc0MGZjYzc4ZjZiYzk3MmQiLCJUaW1lIjoxNjg0NTQxOTcwMjM5LCJpYXQiOjE2ODQ1NDE5NzB9.-fQUjUmWZ8rjx2xBhu6ReSKxwTy54fJy_35IXT7paKU" add={-450} />
+			<IPhone url="http://18.198.41.196" add={-400} />
+			<IPhone url="http://18.193.224.96" add={-350} />
 		</section>
 	);
 }
